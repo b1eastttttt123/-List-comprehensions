72c8474ba605b95b09d6cfd6b2aa282a401b3931
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-04-07 04:31:16 +1000

    Update main.py

--- List comprehensions/Task 3. Price increase/main.py
+++ List comprehensions/Task 3. Price increase/main.py
@@ -1,1 +1,20 @@
+def task(n):
+    score_one = 0
+
+    for num in cost_products:
+        score_one += num
+        percent = score_one * n / 100
+        summ = percent + score_one
+    print(round(summ, 2), end=" ")
+
+cost_products = [float(input(f"The price for {i} product: ")) for i in range(1, 5 + 1)]
+
+one_year_percent = float(input("Promotion for the first year: "))
+two_year_percent = float(input("Promotion for the second year: "))
+
+print("The sum of prices for each year:", end=" ")
+
+task(0)
+task(one_year_percent)
+task(two_year_percent)
 
