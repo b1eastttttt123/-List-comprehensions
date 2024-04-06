8478ed31baa1abda4843c4c42e1f188d39e630d6
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-04-07 03:22:50 +1000

    Update main.py

--- List comprehensions/Task 2. Message/main.py
+++ List comprehensions/Task 2. Message/main.py
@@ -1,1 +1,10 @@
+# Creating an input
+text = input("Enter the text: ")
+add_symbol = input("Enter the additional character: ")
 
+double_characters = [i * 2 for i in text] # for double symbol 
+add_symbol_to_text = [i * 2 + add_symbol for i in text] # add symbol to text 
+
+# Output
+print("List of doubled characters:", double_characters)
+print("Gluing with an additional symbol:", add_symbol_to_text)
