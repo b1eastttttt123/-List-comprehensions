98e76e9cef4bc1e00600218c150a38bd08953539
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-04-07 14:36:55 +1000

    Update main.py

--- List of comprehensions with conditions. The random module/Task 2. Shop/main.py
+++ List of comprehensions with conditions. The random module/Task 2. Shop/main.py
@@ -1,1 +1,8 @@
+original_prices = [1.25, -9.45, 10.22, 3.78, -5.92, 1.16]
 
+for x in original_prices:
+
+    if x < 0:
+        original_prices[original_prices.index(x)] = 0
+
+print("Result:", original_prices)
