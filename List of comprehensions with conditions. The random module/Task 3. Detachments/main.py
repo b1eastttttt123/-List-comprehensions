1f9cb5690c9679fe535dac8472164a1bde794fd7
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-04-07 14:45:40 +1000

    Update main.py

--- List of comprehensions with conditions. The random module/Task 3. Detachments/main.py
+++ List of comprehensions with conditions. The random module/Task 3. Detachments/main.py
@@ -1,1 +1,9 @@
+import random
 
+squad1 = [random.randint(50, 80) for _ in range(10)]
+squad2 = [random.randint(30, 60) for _ in range(10)]
+squad3 = ["Died" if squad1[damage] + squad2[damage] > 100 else "Survived" for damage in range(10)]
+
+print("Damage of the first squad:", squad1)
+print("Damage of the second squad:", squad2)
+print("Status of the third squad:", squad3)
