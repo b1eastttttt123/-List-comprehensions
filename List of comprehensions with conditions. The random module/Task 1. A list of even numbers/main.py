e0e014fb64a3a9106c49b4769d8dfb62214fdc64
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-04-07 14:11:29 +1000

    Update main.py

--- List of comprehensions with conditions. The random module/Task 1. A list of even numbers/main.py
+++ List of comprehensions with conditions. The random module/Task 1. A list of even numbers/main.py
@@ -1,1 +1,8 @@
+import random
 
+start_range = int(input("Enter the initial range: "))
+end_range = int(input("Enter the end range: "))
+
+total_range = [random.randint(start_range, end_range) for _ in range(start_range, end_range)]
+
+print(total_range)
