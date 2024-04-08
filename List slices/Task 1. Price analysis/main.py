3aee39f1b0e102ce3a48be8083e2e903fa9996be
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-04-08 15:00:29 +1000

    Update main.py

--- List slices/Task 1. Price analysis/main.py
+++ List slices/Task 1. Price analysis/main.py
@@ -1,1 +1,17 @@
+import random
 
+original_prices = [random.randint(-100, 100) for _ in range(5)]
+
+new_prices = original_prices
+
+score = 0
+print("Market price Full:", original_prices)
+
+for i in range(len(original_prices)):
+
+    if new_prices[i] < 0:
+        score -= new_prices[i]
+        new_prices[i] = 0
+        
+print("Market price Update:", original_prices)
+print("We've lost:",  score)
