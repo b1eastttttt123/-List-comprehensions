04f9d168999c078ea40975f226d072d657c7a214
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-04-09 00:44:10 +1000

    Update main.py

--- TASKS/Task 5. U-turn/main.py
+++ TASKS/Task 5. U-turn/main.py
@@ -1,8 +1,7 @@
-
 text_user = input('Введите строку: ')
 
 revers = text_user[text_user.find('h') + 1 : text_user.rfind('h')]
 
 text_user = revers[::-1]
 
-print(text_user)
+print("Развёрнутая последовательность между первым и последним h:", text_user)
