b5a4f54ae9d2967d6d81f60d576bbe4114cff6bc
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-04-08 21:33:16 +1000

    Update main.py

--- TASKS/Task 3. Random competitions/main.py
+++ TASKS/Task 3. Random competitions/main.py
@@ -1,1 +1,10 @@
+import random
+import math
 
+first_team = [round(random.uniform(5, 10), 2) for _ in range(20)]
+second_team = [round(random.uniform(5, 10), 2) for _ in range(20)]
+winners = [max(first_team[number], second_team[number]) for number in range(20)]
+
+print("Первая команда:", first_team)
+print("Вторая команда:", second_team)
+print("Победители тура:", winners)
