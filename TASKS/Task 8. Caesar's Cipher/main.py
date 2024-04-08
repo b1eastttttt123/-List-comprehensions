ea92e7054e422832ca8163965030b881c5177c7d
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-04-09 01:12:07 +1000

    Update main.py

--- TASKS/Task 8. Caesar's Cipher/main.py
+++ TASKS/Task 8. Caesar's Cipher/main.py
@@ -1,1 +1,18 @@
+def caesar_cipher(message, shift):
+    encrypted_message = ""
+    for letter in message:
+        if letter.isalpha():
+            if letter.islower():
+                encrypted_message += chr((ord(letter) - ord('а') + shift) % 32 + ord('а'))
+            elif letter.isupper():
+                encrypted_message += chr((ord(letter) - ord('А') + shift) % 32 + ord('А'))
+        else:
+            encrypted_message += letter
+    return encrypted_message
 
+message = input("Введите сообщение: ")
+shift = int(input("Введите сдвиг: "))
+
+encrypted_message = caesar_cipher(message, shift)
+
+print("Зашифрованное сообщение:", encrypted_message)
