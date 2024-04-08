8375e7c845330bc351d492d5e96c02c55fb027db
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-04-08 15:16:16 +1000

    Update main.py

--- List slices/Task 2. Slices/main.py
+++ List slices/Task 2. Slices/main.py
@@ -1,1 +1,9 @@
+nums = [48, -10, 9, 38, 17, 50, -5, 43, 46, 12]
 
+print("Our list of numbers:", nums)
+print("Print the first five items of the list:", nums[:5])
+print("Print the entire list except the last two items:", nums[:-2])
+print("Print all the elements with even indexes:", nums[::2])
+print("Print all the elements with odd indexes:", nums[1::2])
+print("Print all the elements in reverse order:", nums[::-1])
+print("Print all the items in the list one by one in reverse order, starting from the last one:", nums[::-2])
