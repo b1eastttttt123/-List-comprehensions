5587d5e45d4209e3089802df56e6116ae08a4efe
Denis <122338462+b1eastttttt123@users.noreply.github.com>
2024-04-09 01:06:22 +1000

    Update main.py

--- TASKS/Task 7. List of lists/main.py
+++ TASKS/Task 7. List of lists/main.py
@@ -1,1 +1,7 @@
+nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
+
+result = [num2 for num1 in nice_list for num2 in [number for num in num1 for number in num]]
+
+print(result)
+
 
